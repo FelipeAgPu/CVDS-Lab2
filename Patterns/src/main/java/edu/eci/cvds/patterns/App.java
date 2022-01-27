f1affd0e1dca14a835d8fbf6733a88ea320a608e
unknown <juan.aguas@labinfo.is.escuelaing.edu.co>
2022-01-27 09:41:47 -0500

    Avances clase

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,14 @@
+package edu.eci.cvds.patterns;
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( Arrays.toString(args) );
+    }
+}
