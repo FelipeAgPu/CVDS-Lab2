8cc3d3ad7aee12be66bc451f705f99288af24337
diegoruro <diegofernandoruizrojas@gmail.com>
2022-02-02 17:56:43 -0500

    Shapefactory y gitignore añadidos

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,5 +1,24 @@
 package edu.eci.cvds.patterns.shapes;
 import edu.eci.cvds.patterns.shapes.*;
+import edu.eci.cvds.patterns.shapes.concrete.*;
 
 public class ShapeFactory {
+    public static Shape create(RegularShapeType type){
+        Shape selectedShape = null;
+        switch (type) {
+            case Triangle:
+                selectedShape = new Triangle();
+                break;
+            case Quadrilateral:
+                selectedShape = new Quadrilateral();
+                break;
+            case Pentagon:
+                selectedShape = new Pentagon();
+                break;
+            case Hexagon:
+                selectedShape = new Hexagon();
+                break;
+        }
+        return selectedShape;
+    }
 }
